bcd618f791004351860d7749b336b88986a5f9d2
Mark Delany <d5e@xray.emu.st>
2019-10-05 06:41:48 +1000

    Bump version to 0.0.2 - See Changelog for details

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,6 @@
 # cslb Change Log
+### v0.0.2 -- 2019-10-05
+  * Fixed typos and clarified comments - no bug fixes or functionality changes
+  * Bumped travis upper go version to 1.13.1
 ### v0.0.1 -- 2019-07-15
   * Initial public release.
--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package cslb
 
 const (
 	// Version is auto-generated from ChangeLog.md
-	Version = "v0.0.1"
+	Version = "v0.0.2"
 	// ReleaseDate is also auto-generated from ChangeLog.md
-	ReleaseDate = "2019-07-15"
+	ReleaseDate = "2019-10-05"
 )
